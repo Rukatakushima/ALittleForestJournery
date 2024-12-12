a087c25d7f5584d506e493440527bf43929a9833
Ануфриева Валерия 211-727 <broken.mind633@gmail.com>
2024-12-12 05:09:15 +0300

    Escape Puzzle Game
    Completed
    
    (Maybe make a more comfortable generator)

---
+++ Assets/Escape/Scripts/EscapeLevel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EscapeLevel", menuName = "EscapeLevel", order = 0)]
+public class EscapeLevel : ScriptableObject 
+{
+   public int Row;
+   public int Col;
+   public Piece WinPiece;
+   public List<Piece> Pieces;
+}
+
+[Serializable]
+public struct Piece
+{
+    public int Id;
+    public bool IsVertical;
+    public int Size;
+    public Vector2Int Start;
+}
