1212a221fa19cd84b1c0acb7a0334984d464f0e1
Ануфриева Валерия 211-727 <broken.mind633@gmail.com>
2025-01-27 18:50:44 +0300

    Puzzle Connect COPYPASTE

--- Assets/PuzzleConnect/Scripts/ConnectGameplayManager.cs
+++ Assets/PuzzleConnect/Scripts/ConnectGameplayManager.cs
@@ -1,16 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 namespace Connect
 {
-    public class ConnectGameplayManager : MonoBehaviour
+    public class GameplayManager : MonoBehaviour
     {
         #region START_METHODS
 
         #region START_VARIABLES
-        public static ConnectGameplayManager Instance;
-        [HideInInspector] public bool hasGameFinished;
+        public static GameplayManager Instance;
+
+        public bool hasGameFinished;
+
 
         private void Awake()
         {
@@ -18,60 +21,58 @@ namespace Connect
 
             hasGameFinished = false;
 
-            // CurrentLevelData = DefaultLevel;
-
             SpawnBoard();
+
             SpawnNodes();
+
         }
+
         #endregion
 
         #region BOARD_SPAWN
 
         [SerializeField] private SpriteRenderer _boardPrefab, _bgCellPrefab;
-        [SerializeField] public int CurrentStage = 5;
+
         private void SpawnBoard()
         {
-            int currentLevelSize = CurrentStage;
+            int currentLevelSize = 5;
 
-            //инициализация подложки: префаб, положение (делим размер на 2, чтобы разместить в середине), поворот
             var board = Instantiate(_boardPrefab,
                 new Vector3(currentLevelSize / 2f, currentLevelSize / 2f, 0f),
                 Quaternion.identity);
 
-            // добавлеяем к размеру "обводку\рамку"
-            board.size = new Vector2(currentLevelSize + 0.1f, currentLevelSize + 0.1f);
+            board.size = new Vector2(currentLevelSize + 0.08f, currentLevelSize + 0.08f);
 
             for (int i = 0; i < currentLevelSize; i++)
             {
                 for (int j = 0; j < currentLevelSize; j++)
                 {
-                    Instantiate(_bgCellPrefab,
-                        new Vector3(i + 0.5f, j + 0.5f, 0f),
-                        Quaternion.identity);
+                    Instantiate(_bgCellPrefab, new Vector3(i + 0.5f, j + 0.5f, 0f), Quaternion.identity);
                 }
             }
 
-            Camera.main.orthographicSize = currentLevelSize - 1f;// + 2f;
+            Camera.main.orthographicSize = currentLevelSize + 2f;
             Camera.main.transform.position = new Vector3(currentLevelSize / 2f, currentLevelSize / 2f, -10f);
+
         }
 
         #endregion
 
-        #region NODES_SPAWN
+        #region NODE_SPAWN
 
         [SerializeField] public ConnectLevelData CurrentLevelData;
-        [SerializeField] private ConnectNode _nodePrefab;
-        private List<ConnectNode> _nodes;
+        [SerializeField] private Node _nodePrefab;
+        private List<Node> _nodes;
 
-        public Dictionary<Vector2Int, ConnectNode> _nodeGrid;
+        public Dictionary<Vector2Int, Node> _nodeGrid;
 
         private void SpawnNodes()
         {
-            _nodes = new List<ConnectNode>();
-            _nodeGrid = new Dictionary<Vector2Int, ConnectNode>();
+            _nodes = new List<Node>();
+            _nodeGrid = new Dictionary<Vector2Int, Node>();
 
-            int currentLevelSize = CurrentStage;
-            ConnectNode spawnedNode;
+            int currentLevelSize = 5;
+            Node spawnedNode;
             Vector3 spawnPos;
 
             for (int i = 0; i < currentLevelSize; i++)
@@ -83,7 +84,7 @@ namespace Connect
                     spawnedNode.Init();
 
                     int colorIdForSpawnedNode = GetColorId(i, j);
-                    //окрас point в цвет, который будем соединять
+
                     if (colorIdForSpawnedNode != -1)
                     {
                         spawnedNode.SetColorForPoint(colorIdForSpawnedNode);
@@ -93,12 +94,12 @@ namespace Connect
                     _nodeGrid.Add(new Vector2Int(i, j), spawnedNode);
                     spawnedNode.gameObject.name = i.ToString() + j.ToString();
                     spawnedNode.Pos2D = new Vector2Int(i, j);
+
                 }
             }
 
-            //установка сетки: сетка = nodes, у каждого node есть список offsetPos, т.е. 4 edge (верх, них, лево, право)
             List<Vector2Int> offsetPos = new List<Vector2Int>()
-            { Vector2Int.up, Vector2Int.down,Vector2Int.left, Vector2Int.right};
+            {Vector2Int.up,Vector2Int.down,Vector2Int.left,Vector2Int.right };
 
             foreach (var item in _nodeGrid)
             {
@@ -112,6 +113,7 @@ namespace Connect
                 }
             }
 
+
         }
 
         public List<Color> NodeColors;
@@ -123,29 +125,39 @@ namespace Connect
 
             for (int colorId = 0; colorId < edges.Count; colorId++)
             {
-                if (edges[colorId].StartPoint == point || edges[colorId].EndPoint == point) return colorId;
+                if (edges[colorId].StartPoint == point ||
+                    edges[colorId].EndPoint == point)
+                {
+                    return colorId;
+                }
             }
 
             return -1;
         }
 
-        // public Color GetHighlightColor(int colorID)
-        // {
-        //     Color result = NodeColors[colorID];
-        //     result.a = 0.4f; //Alpha component of the color (0 is transparent, 1 is opaque).
-        //     return result;
-        // }
+        public Color GetHighLightColor(int colorID)
+        {
+            Color result = NodeColors[colorID % NodeColors.Count];
+            result.a = 0.4f;
+            return result;
+        }
+
 
         #endregion
+
         #endregion
 
         #region UPDATE_METHODS
 
-        private ConnectNode startNode;
+        private Node startNode;
 
         private void Update()
         {
-            if (hasGameFinished) return;
+            if (hasGameFinished)
+            {
+                GameFinished();
+                return;
+            }
 
             if (Input.GetMouseButtonDown(0))
             {
@@ -155,21 +167,25 @@ namespace Connect
 
             if (Input.GetMouseButton(0))
             {
+
                 Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
                 RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
 
                 if (startNode == null)
                 {
-                    if (hit && hit.collider.gameObject.TryGetComponent(out ConnectNode tNode)
-                    && tNode.IsClickable)
+                    if (hit && hit.collider.gameObject.TryGetComponent(out Node tNode)
+                        && tNode.IsClickable)
                     {
                         startNode = tNode;
                     }
+
                     return;
                 }
 
-                if (hit && hit.collider.gameObject.TryGetComponent(out ConnectNode tempNode) && startNode != tempNode)
+
+                if (hit && hit.collider.gameObject.TryGetComponent(out Node tempNode)
+                    && startNode != tempNode)
                 {
                     if (startNode.colorId != tempNode.colorId && tempNode.IsEndNode)
                     {
@@ -190,13 +206,19 @@ namespace Connect
             }
 
         }
+
         #endregion
 
-        #region WIN_CONDITIONS
+        #region WIN_CONDITION
 
         private void CheckWin()
         {
             bool IsWinning = true;
+
+            foreach (var item in _nodes)
+            {
+                item.SolveHighlight();
+            }
 
             foreach (var item in _nodes)
             {
@@ -207,15 +229,19 @@ namespace Connect
                 }
             }
 
+            // GameManager.Instance.UnlockLevel();
+
             hasGameFinished = true;
-            StartCoroutine(GameFinished());
+            Debug.Log(hasGameFinished);
+            GameFinished();
         }
-        
+
+        #endregion
+
         private IEnumerator GameFinished()
         {
             yield return new WaitForSeconds(1f);
             UnityEngine.SceneManagement.SceneManager.LoadScene(0);
         }
-        #endregion
     }
 }
