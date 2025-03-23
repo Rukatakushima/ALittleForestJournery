6d17b87b98af575d08a305335bf67de75053def3
Ануфриева Валерия 211-727 <broken.mind633@gmail.com>
2025-03-23 16:37:03 +0300

    Puzzle Number Link #9
    - Refactored the code

--- Assets/Puzzles/NumberLink/Scripts/NumberLinkCameraController.cs
+++ Assets/Puzzles/NumberLink/Scripts/NumberLinkCameraController.cs
@@ -6,16 +6,6 @@ namespace NumberLink
     {
         public void SetupCamera(float width, Vector3 backgroundPosition)
         {
-            /*
-            
-            Vector3 bgPos = new Vector3(
-                width / 2f - cellSize / 2f - levelGap / 2f,
-                height / 2f - cellSize / 2f - levelGap / 2f,
-                0
-                );
-                
-            float width = (cellSize + cellGap) * levelData.col - cellGap + levelGap;
-            */
             Camera.main.orthographicSize = width * cameraSizeController;
             Camera.main.transform.position = new Vector3(backgroundPosition.x, backgroundPosition.y, cameraPositionZ);
         }
